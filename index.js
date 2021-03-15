c3232b41c1e77657f3cf02f5bd0df8f8e47bddd8
BajajPratyush <pratyush.bajaj123@gmail.com>
2021-03-15 13:06:54 +0530

    added alert to every button

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function handleClick() {
     alert("I got clicked!");
 }
-document.querySelector(".w").addEventListener("click", handleClick);
+for (var i = 0; i < document.querySelectorAll(".drum").length; ++i) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+}
